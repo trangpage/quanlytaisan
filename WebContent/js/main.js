545e847b2fd19d50c1356042b87cf111b2973a20
12130329 <12130329@st.hcmuaf.edu.vn>
2016-03-24 15:11:30 -0700

    lan dau tien

---
+++ WebContent/js/main.js
@@ -0,0 +1,12 @@
+function ViecLamChuyenMon() {
+	window.location ="LaoDongPhoThong.html";
+}
+function ldpt() {
+	window.location ="LaoDongPhoThong.html";
+}
+function vlsv() {
+	window.location ="LaoDongPhoThong.html";
+}
+function nhatuyendung() {
+	window.location ="LaoDongPhoThong.html";
+}
